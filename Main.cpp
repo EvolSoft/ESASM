872e20c4a9ef6e5f984533f9fd3dfbdfaa286821
Flavius12 <info@evolsoft.tk>
2015-05-31 16:57:06 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,12 @@
+/*
+ * ESASM (v1.0) by EvolSoft
+ * Developer: EvolSoft (Flavius12)
+ * Website: http://www.evolsoft.tk
+ * Date: 31/05/2015 03:56 PM (UTC)
+ * Copyright & License: (C) 2015 EvolSoft
+ * Licensed under MIT (https://github.com/EvolSoft/ESASM/blob/master/LICENSE)
+ */
+
 #include <iostream>
 #include <io.h>
 #include <sys/stat.h>
