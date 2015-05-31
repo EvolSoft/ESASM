956ab34b2a0adf963e3593b9871dea5e514e60b0
Flavius12 <info@evolsoft.tk>
2015-05-31 16:56:53 +0200

    Create parser.h

---
+++ parser/parser.h
@@ -0,0 +1,13 @@
+/*
+ * ESASM (v1.0) by EvolSoft
+ * Developer: EvolSoft (Flavius12)
+ * Website: http://www.evolsoft.tk
+ * Date: 31/05/2015 03:56 PM (UTC)
+ * Copyright & License: (C) 2015 EvolSoft
+ * Licensed under MIT (https://github.com/EvolSoft/ESASM/blob/master/LICENSE)
+ */
+
+class Parser {
+    public:
+        Parser(char* filename, int type);
+};
