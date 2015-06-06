124d0cc1ac173f3c82e713d80afd6aa593f78446
Flavius12 <info@evolsoft.tk>
2015-06-06 17:33:16 +0200

    Create compiler.cpp

---
+++ compiler/compiler.cpp
@@ -0,0 +1,33 @@
+/*
+ * ESASM (v1.0.0) by EvolSoft
+ * Developer: EvolSoft
+ * Website: http://www.evolsoft.tk
+ * Date: 06/06/2015 04:33 PM (UTC)
+ * Copyright & License: (C) 2015 EvolSoft
+ * Licensed under MIT (https://github.com/EvolSoft/ESASM/blob/master/LICENSE)
+ */
+
+#include <string>
+#include "compiler.h"
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <vector>
+
+using namespace std;
+
+int Compiler::compile(string outfile, int type){
+    ofstream out(outfile.c_str());
+    if(out.fail()){
+        return 1;
+    }else{
+        ostream_iterator<unsigned char> output_iterator(out, "");
+        copy(bs.begin(), bs.end(), output_iterator);
+        return 0;
+    }
+}
+
+void Compiler::addByte(char byte){
+    bs.push_back(byte);
+}
+
