2376b4d03ea0ddf924c681585d56bc56854ec9bf
Flavius12 <info@evolsoft.tk>
2015-06-06 17:04:09 +0200

    Added AAA opcode

---
+++ parser/opcodes/aaa.h
@@ -0,0 +1,27 @@
+/*
+ * ESASM (v1.0.0) by EvolSoft
+ * Developer: EvolSoft
+ * Website: http://www.evolsoft.tk
+ * Date: 06/06/2015 04:03 PM (UTC)
+ * Copyright & License: (C) 2015 EvolSoft
+ * Licensed under MIT (https://github.com/EvolSoft/ESASM/blob/master/LICENSE)
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class aaa {
+public:
+    aaa(Parser* parser, vector<string> line, string filename, int fileline);
+};
+
+aaa::aaa(Parser* parser, vector<string> line, string filename, int fileline){
+    if(line.size() > 1){
+        cout << filename << ":" << fileline << ", Error: Invalid usage";
+        parser->status = 1;
+    }else{
+        Compiler::addByte(0x37);
+    }
+}
