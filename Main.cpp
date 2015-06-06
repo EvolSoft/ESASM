f8f0fbe185c7122a4e9cc295462994bf1a8f807a
Flavius12 <info@evolsoft.tk>
2015-06-06 16:58:32 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,14 @@
 /*
- * ESASM (v1.0) by EvolSoft
- * Developer: EvolSoft (Flavius12)
+ * ESASM (v1.0.0) by EvolSoft
+ * Developer: EvolSoft
  * Website: http://www.evolsoft.tk
- * Date: 31/05/2015 03:56 PM (UTC)
+ * Date: 06/06/2015 03:57 PM (UTC)
  * Copyright & License: (C) 2015 EvolSoft
  * Licensed under MIT (https://github.com/EvolSoft/ESASM/blob/master/LICENSE)
  */
 
 #include <iostream>
+#include <string>
 #include <io.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -16,12 +17,12 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-    cout << "EvolSoft ESASM v1.0\n";
-    cout << "(c) 2015 EvolSoft. Licensed under MIT\n";
+    Parser parser;
+    Compiler compiler;
+    string rawname;
+    cout << "EvolSoft ESASM v1.0.0\n";
+    cout << "(C) 2015 EvolSoft. Licensed under MIT\n";
     cout << "http://www.evolsoft.tk\n\n";
-    /*FILE * file = fopen("test.txt", "w");
-    fputc(0x01, file);
-    fclose(file);*/
     bool has_c = false; //Doesn't repeat commands
     bool has_o = false; //Doesn't repeat commands
     for(int i = 0; i < argc; i++){
@@ -30,9 +31,11 @@ int main(int argc, char* argv[]){
                 has_c = true;
                 struct stat st;
                 if(stat(argv[i+1], &st) == 0 && S_ISREG(st.st_mode)){
-                    Parser* parser = new Parser(argv[i+1], 0);
+                    parser.parse(argv[i+1], 0);
+                    int lastindex = string(argv[i+1]).find_last_of(".");
+                    rawname = string(argv[i+1]).substr(0, lastindex);
                 }else{
-                    cout << "Source file not found";
+                    cout << "Error: Source file not found";
                     return 1; //Return I/O related error
                 }
             }
@@ -40,8 +43,14 @@ int main(int argc, char* argv[]){
         if(strstr(argv[i], "-o") || strstr(argv[i], "-O")){
             if(!has_o){
                 has_o = true;
-                //Do
+                rawname = argv[i+1];
             }
+        }
+    }
+    if(parser.getStatus() == 0 && has_c){
+        int ret = compiler.compile(rawname, 1);
+        if(ret == 0){
+            cout << "Success: Compilation complete!";
         }
     }
 }
