13bc35bf2f0ea2b897a1037634c87652bb11f88f
Flavius12 <info@evolsoft.tk>
2015-06-07 12:44:16 +0200

    Added comment support

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -2,7 +2,7 @@
  * ESASM (v1.0.0) by EvolSoft
  * Developer: EvolSoft
  * Website: http://www.evolsoft.tk
- * Date: 06/06/2015 04:01 PM (UTC)
+ * Date: 07/06/2015 11:43 AM (UTC)
  * Copyright & License: (C) 2015 EvolSoft
  * Licensed under MIT (https://github.com/EvolSoft/ESASM/blob/master/LICENSE)
  */
@@ -17,6 +17,7 @@
 #include "parser.h"
 //Opcodes
 #include "opcodes\aaa.h"
+#include "opcodes\aad.h"
 
 using namespace std;
 
@@ -36,6 +37,21 @@ vector<string> split(string line) {
     return elements;
 }
 
+string removeComments(string line){
+    bool is_string = false;
+    int pos = line.length();
+    for(int i = 0; i <= line.length(); i++){
+        if((line[i] == '\"' && line[i] == '\'') && line[i-1] != '\\'){
+            is_string = !is_string;
+        }
+        if(line[i] == ';' && !is_string){
+            pos = i;
+            break;
+        }
+    }
+    return line.substr(0, pos);
+}
+
 void Parser::parse(string filename, int type){
     ifstream file(filename.c_str());
     string line;
@@ -45,14 +61,17 @@ void Parser::parse(string filename, int type){
         while(getline(file, line)){
             fileline += 1;
             line = trim(line);
+            line = removeComments(line);
             vector<string> el = split(line);
             transform(el[0].begin(), el[0].end(), el[0].begin(), ::tolower);
             if(el[0].compare("aaa") == 0){
-               aaa aaa_ = aaa(this, el, filename, fileline);
+                aaa aaa_ = aaa(this, el, filename, fileline);
+            }else if(el[0].compare("aad") == 0){
+                aad aad_ = aad(this, el, filename, fileline);
             }
         }
     }else{
-        cout << "Error: Can't read source file. Permission denied.";
+        cout << "\nError: Can't read source file. Permission denied.";
         status = 1;
     }
 }
