abb321ac012590078339a40632124f76d5b22028
Flavius12 <info@evolsoft.tk>
2015-06-07 12:14:27 +0200

    Update parser.h

--- parser/parser.h
+++ parser/parser.h
@@ -1,13 +1,20 @@
 /*
- * ESASM (v1.0) by EvolSoft
- * Developer: EvolSoft (Flavius12)
+ * ESASM (v1.0.0) by EvolSoft
+ * Developer: EvolSoft
  * Website: http://www.evolsoft.tk
- * Date: 31/05/2015 03:56 PM (UTC)
+ * Date: 07/06/2015 11:14 AM (UTC)
  * Copyright & License: (C) 2015 EvolSoft
  * Licensed under MIT (https://github.com/EvolSoft/ESASM/blob/master/LICENSE)
  */
 
+#include <string>
+#include "../compiler/compiler.h"
+
+using namespace std;
+
 class Parser {
-    public:
-        Parser(char* filename, int type);
+public:
+    void parse(string filename, int type);
+    int status;
+    int getStatus();
 };
