d682446e899c5a4d13e4a68441629e5a674f488b
Flavius12 <info@evolsoft.tk>
2015-06-07 12:15:34 +0200

    Fixed binary output

--- compiler/compiler.cpp
+++ compiler/compiler.cpp
@@ -2,7 +2,7 @@
  * ESASM (v1.0.0) by EvolSoft
  * Developer: EvolSoft
  * Website: http://www.evolsoft.tk
- * Date: 06/06/2015 04:33 PM (UTC)
+ * Date: 07/06/2015 11:15 AM (UTC)
  * Copyright & License: (C) 2015 EvolSoft
  * Licensed under MIT (https://github.com/EvolSoft/ESASM/blob/master/LICENSE)
  */
@@ -11,13 +11,14 @@
 #include "compiler.h"
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <iterator>
 #include <vector>
 
 using namespace std;
 
 int Compiler::compile(string outfile, int type){
-    ofstream out(outfile.c_str());
+    ofstream out(outfile.c_str(), ios::binary);
     if(out.fail()){
         return 1;
     }else{
